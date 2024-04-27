8fad2f7673aed51e4effe304197bafa5c6f8cb13
Emma Larsson <emma940112@gmail.com>
2024-04-27 08:04:58 +0200

    Skapat funktionalitet för att kunna sortera kurser i bokstavsordning

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -12,12 +12,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  //Properties
   courselist: Course[] = [];
+  //Array som lagrar de kurser som finns kvar efter filtreringen
   filteredCourse: Course[] = [];
   filterValue: string = "";
+  sortBy: string = "";
 
   constructor(private courseservice: CourseService) { }
 
+  //Metod som körs när komponenten initialiseras
   ngOnInit() {
     this.courseservice.getCourses().subscribe(data => {
       this.courselist = data;
@@ -25,10 +29,33 @@ export class CoursesComponent {
     });
   }
 
+  //Methods
+  //Metod som körs när användaren skriver in något i input-fältet
   applyFilter(): void {
     this.filteredCourse = this.courselist.filter((course) =>
-    course.code.toLowerCase().includes(this.filterValue.toLowerCase()) || course.coursename.toLowerCase().includes(this.filterValue.toLowerCase())
+      course.code.toLowerCase().includes(this.filterValue.toLowerCase()) || course.coursename.toLowerCase().includes(this.filterValue.toLowerCase())
     );
+  }
+  //Metod som körs vid klick på "Kurskod", "Kursnamn" eller "Progression"
+  sortCourse(column: string): void {
+    //Om samma kolumn klickas på två gången, vänd sorteringen
+    if (this.sortBy === column) {
+      this.filteredCourse.reverse();
+    } else {
+      //Annars sortera kurserna efter den klickade kolumnen
+      this.sortBy = column;
+      this.filteredCourse.sort((a, b) => {
+        if (column === 'code') {
+          return a.code.localeCompare(b.code);
+        } else if (column === 'name') {
+          return a.coursename.localeCompare(b.coursename);
+        } else if (column === 'progression') {
+          return a.progression.localeCompare(b.progression);
+        }
+        //Om kolumnen inte matchar något av värdena, behåll ordningen
+        return 0;
+      });
+    }
   }
 }
 
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -7,11 +7,17 @@ import { Course } from '../model/course';
   providedIn: 'root'
 })
 export class CourseService {
-
+  //Properties
+  //URL till kursfil
   private url: string = "https://webbutveckling.miun.se/files/ramschema_ht23.json";
 
+  //Constructor
   constructor(private http: HttpClient) { }
+
+  //Methods
+  //Metod för att hämta kurser
   getCourses(): Observable<Course[]> {
+    //Ett HTTP GET-anrop till den angivna URL:en
     return this.http.get<Course[]>(this.url);
   }
 }
